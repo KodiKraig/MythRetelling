3211902851bbb975a822a0bf57119de55433d023
Cody Craig <cody.craig@aol.com>
2018-04-01 04:17:08 -0400

    Initial Commit

---
+++ MythRetelling/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MythRetelling
+//
+//  Created by Cody Craig on 4/1/18.
+//  Copyright © 2018 Cody Craig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
